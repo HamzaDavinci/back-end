d8efe290e86e8adc95fce674cab8c23354ca4bfa
HamzaDavinci <99075941@mydavinci.nl>
2025-05-15 00:07:29 +0200

    .

--- forms/handleForms.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    $naam = htmlspecialchars($_POST["naam"], ENT_QUOTES, 'UTF-8');
-    $email = htmlspecialchars($_POST["email"], ENT_QUOTES, 'UTF-8');
-
-    echo "Naam: $naam<br>";
-    echo "Je email is: $email";
-
-?>
